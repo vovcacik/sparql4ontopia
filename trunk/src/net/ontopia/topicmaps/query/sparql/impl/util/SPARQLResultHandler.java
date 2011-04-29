6b8133e0be94d296ae64d6107e3ab63894b52b4a
v.ovcacik@gmail.com <v.ovcacik@gmail.com@87c0a53f-d6ed-43f0-9daf-9f6f73cdfd02>
2011-04-29 08:02:06 +0000

    Moving some methods from SPARQLResultHandler to SparqlTupleQueryResult.

--- trunk/src/net/ontopia/topicmaps/query/sparql/impl/util/SPARQLResultHandler.java
+++ trunk/src/net/ontopia/topicmaps/query/sparql/impl/util/SPARQLResultHandler.java
@@ -3,7 +3,6 @@ package net.ontopia.topicmaps.query.sparql.impl.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.TupleQueryResultHandler;
 import org.openrdf.query.TupleQueryResultHandlerException;
@@ -30,34 +29,22 @@ public class SPARQLResultHandler implements TupleQueryResultHandler {
 		columnNames = bindingNames;
 	}
 
-	public String getColumnName(int index) {
-		return columnNames.get(index);
-	}
-
-	public String[] getColumnNames() {
-		return columnNames.toArray(new String[0]);
-	}
-
-	public int getIndex(String columnName) {
-		if (columnNames.contains(columnName)) {
-			return columnNames.indexOf(columnName);
-		} else {
-			return -1;
-		}
-	}
-
-	public String getValue(String columnName, int rowIndex) {
-		BindingSet row = rows.get(rowIndex);
-		Value value = row.getValue(columnName);
-		return value.stringValue();
-	}
-
-	public int getHeight() {
-		return rows.size();
-	}
-
 	public void close() {
 		columnNames = null;
 		rows = null;
+
 	}
+
+	// interface metody
+	public List<String> getColumnNames() {
+		// TODO return kopii
+		return columnNames;
+	}
+
+	public List<BindingSet> getRows() {
+		// TODO returnkopii
+		// TODO list<Object>??
+		return rows;
+	}
+
 }
