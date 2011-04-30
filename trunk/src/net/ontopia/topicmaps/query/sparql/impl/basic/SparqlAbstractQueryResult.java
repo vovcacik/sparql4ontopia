a5cbbbb50ab3d180fa7043a98ad64938fa9f71b8
v.ovcacik@gmail.com <v.ovcacik@gmail.com@87c0a53f-d6ed-43f0-9daf-9f6f73cdfd02>
2011-04-30 08:37:13 +0000

    Javadoc added

--- trunk/src/net/ontopia/topicmaps/query/sparql/impl/basic/SparqlAbstractQueryResult.java
+++ trunk/src/net/ontopia/topicmaps/query/sparql/impl/basic/SparqlAbstractQueryResult.java
@@ -3,23 +3,51 @@ package net.ontopia.topicmaps.query.sparql.impl.basic;
 import java.util.List;
 
 import net.ontopia.topicmaps.query.core.QueryResultIF;
+import net.ontopia.topicmaps.query.sparql.impl.util.OntopiaResultHandler;
 
+/**
+ * Abstract class providing basic implementation of some methods used in {@link QueryResultIF}.
+ * 
+ * @see {@link SparqlTupleQueryResult}
+ * @see {@link SparqlGraphQueryResult}
+ * 
+ * @author Vlastimil OvË·ËÌk
+ * 
+ */
 public abstract class SparqlAbstractQueryResult implements QueryResultIF {
 
+	/**
+	 * Type depends on {@link OntopiaResultHandler}.
+	 * 
+	 * @see {@link OntopiaResultHandler} getColumnNames() method
+	 */
 	protected List<String> columnNames;
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 */
 	public String getColumnName(int ix) {
 		return columnNames.get(ix);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public String[] getColumnNames() {
 		return columnNames.toArray(new String[0]);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public int getIndex(String colname) {
 		return columnNames.indexOf(colname);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public Object[] getValues() {
 		int size = columnNames.size();
 		Object[] row = new Object[size];
@@ -29,12 +57,18 @@ public abstract class SparqlAbstractQueryResult implements QueryResultIF {
 		return row;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public Object[] getValues(Object[] values) {
 		Object[] row = getValues();
 		System.arraycopy(row, 0, values, 0, row.length);
 		return values;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public int getWidth() {
 		return columnNames.size();
 	}
