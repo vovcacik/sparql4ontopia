fb9d4fe756d47ca6aa5de4e566f335f3eca7f628
v.ovcacik@gmail.com <v.ovcacik@gmail.com@87c0a53f-d6ed-43f0-9daf-9f6f73cdfd02>
2011-04-30 14:33:53 +0000

    Extending test coverage. Added testing of queries with base URI.

--- trunk/test/net/ontopia/topicmaps/query/sparql/impl/basic/AllTests.java
+++ trunk/test/net/ontopia/topicmaps/query/sparql/impl/basic/AllTests.java
@@ -5,6 +5,10 @@ import junit.framework.TestSuite;
 
 public class AllTests {
 
+	// Edit the path to match your environment
+	static String ITALIAN_OPERA_PATH = "c:/Program Files/Eclipse/workspace/sparql4ontopia/resources/ItalianOpera.ltm";
+	static String ITALIAN_OPERA_BASE = "http://psi.ontopedia.net/";
+
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for net.ontopia.topicmaps.query.sparql.impl.basic");
 		// $JUnit-BEGIN$
--- trunk/test/net/ontopia/topicmaps/query/sparql/impl/basic/SparqlQueryProcessorFactoryTest.java
+++ trunk/test/net/ontopia/topicmaps/query/sparql/impl/basic/SparqlQueryProcessorFactoryTest.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import junit.framework.TestCase;
 import net.ontopia.topicmaps.core.TopicMapIF;
-import net.ontopia.topicmaps.query.core.QueryProcessorIF;
 import net.ontopia.topicmaps.utils.ltm.LTMTopicMapReader;
 
 import org.junit.After;
@@ -13,14 +12,13 @@ import org.junit.Test;
 
 public class SparqlQueryProcessorFactoryTest extends TestCase {
 
-	private final String ITALIAN_OPERA_PATH = "c:/topicmaps/ontopia-5.1.3/apache-tomcat/webapps/omnigator/WEB-INF/topicmaps/ItalianOpera.ltm";
 	private TopicMapIF tm;
 	private SparqlQueryProcessorFactory factory;
 
 	@Before
 	public void setUp() throws Exception {
 		try {
-			LTMTopicMapReader reader = new LTMTopicMapReader(new File(ITALIAN_OPERA_PATH));
+			LTMTopicMapReader reader = new LTMTopicMapReader(new File(AllTests.ITALIAN_OPERA_PATH));
 			this.tm = reader.read();
 		} catch (java.io.IOException e) {
 			System.err.println("Error reading topic map: " + e);
@@ -30,13 +28,8 @@ public class SparqlQueryProcessorFactoryTest extends TestCase {
 
 	@After
 	public void tearDown() throws Exception {
-		// TODO add
-	}
-
-	@Test
-	public void testCreateQueryProcessor() {
-		QueryProcessorIF processor = factory.createQueryProcessor(tm, null, null);
-		assertNotNull(processor);
+		factory = null;
+		tm = null;
 	}
 
 	@Test
