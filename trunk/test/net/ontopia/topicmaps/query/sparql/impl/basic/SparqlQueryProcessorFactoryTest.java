eb3f3a003ed44d017ce563ef9c32ff6b3d1f52cc
v.ovcacik@gmail.com <v.ovcacik@gmail.com@87c0a53f-d6ed-43f0-9daf-9f6f73cdfd02>
2011-05-02 07:27:48 +0000

    Minor change.

--- trunk/test/net/ontopia/topicmaps/query/sparql/impl/basic/SparqlQueryProcessorFactoryTest.java
+++ trunk/test/net/ontopia/topicmaps/query/sparql/impl/basic/SparqlQueryProcessorFactoryTest.java
@@ -1,10 +1,6 @@
 package net.ontopia.topicmaps.query.sparql.impl.basic;
 
-import java.io.File;
-
 import junit.framework.TestCase;
-import net.ontopia.topicmaps.core.TopicMapIF;
-import net.ontopia.topicmaps.utils.ltm.LTMTopicMapReader;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,24 +8,16 @@ import org.junit.Test;
 
 public class SparqlQueryProcessorFactoryTest extends TestCase {
 
-	private TopicMapIF tm;
 	private SparqlQueryProcessorFactory factory;
 
 	@Before
 	public void setUp() throws Exception {
-		try {
-			LTMTopicMapReader reader = new LTMTopicMapReader(new File(AllTests.ITALIAN_OPERA_PATH));
-			this.tm = reader.read();
-		} catch (java.io.IOException e) {
-			System.err.println("Error reading topic map: " + e);
-		}
 		factory = new SparqlQueryProcessorFactory();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		factory = null;
-		tm = null;
 	}
 
 	@Test
